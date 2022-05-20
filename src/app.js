de1188f9d331cfe34f471898991a9538acc625a4
Sara Cook <saracook087@gmail.com>
2022-05-20 17:24:14 +0100

    stuck on multiply isNan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,80 @@
 const express = require('express');
+const { add, subtract, multiply } = require('../lib/numbers');
+const {
+  sayHello,
+  lowercase,
+  uppercase,
+  firstCharacter,
+  firstCharacters,
+} = require('../lib/strings');
 
 const app = express();
+app.use(express.json());
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-character/:string', (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (!req.query.length) {
+    req.query.length = 1;
+  }
+  res.json({ result: firstCharacters(req.params.string, req.query.length) });
+});
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+  return res.status(200).json({ result: add(a, b) });
+});
+
+app.get('/numbers/subtract/:a/from/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+  return res.status(200).json({ result: subtract(b, a) });
+});
+
+// help?!??!?!?!?!
+app.post('/numbers/multiply/', (req, res) => {
+  const { a } = req.body;
+  const { b } = req.body;
+
+  if (!a || !b) {
+    return res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+  return res.status(200).json({ result: multiply(a, b) });
+});
+
+// booleans
+
+app.get('/booleans/negate', (req, res) => {
+  const valueTrue = req.body.value;
+  if (valueTrue === true) {
+    return res.status(200).json({ result: false });
+  }
+});
 
 module.exports = app;
